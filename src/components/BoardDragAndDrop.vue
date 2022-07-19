b0f55c181ae0cbff20dbcac57deaa5077833a8b3
Fernando Zanchetta <fernandozanca94@gmail.com>
2022-07-18 21:02:14 -0300

    exercise 6 - create new tasks and board features

--- src/components/BoardDragAndDrop.vue
+++ src/components/BoardDragAndDrop.vue
@@ -1,31 +1,61 @@
-<script setup>
-import { watch, reactive, toRaw } from "vue";
+<script setup lang="ts">
 import { cloneDeep } from "lodash-es";
-import draggable from "vuedraggable";
+import { reactive, watch, toRaw } from "vue";
+import type { Board, Column, Task } from "@/types";
 import { v4 as uuidv4 } from "uuid";
-const props = defineProps({
-  board: Object,
-  tasks: Array,
-});
-const emit = defineEmits(["update"]);
+import draggable from "vuedraggable";
+
+import { useAlerts } from "@/stores/alerts";
+const alerts = useAlerts();
+
+const props = defineProps<{
+  board: Board;
+  tasks: Task[];
+  addTask(task: Partial<Task>): Task;
+}>();
+
+const emit = defineEmits<{
+  (e: "update", payload: Partial<Board>): void;
+}>();
+
 const tasks = reactive(cloneDeep(props.tasks));
 const board = reactive(cloneDeep(props.board));
-const columns = reactive(JSON.parse(board.order));
-const addColumn = () =>
-  columns.push({ id: uuidv4(), title: "New column", taskIds: [] });
-watch(columns, () =>
+const columns = reactive<Column[]>(JSON.parse(board.order as string));
+
+const addColumn = () => {
+  columns.push({
+    id: uuidv4(),
+    title: "New Column",
+    taskIds: [],
+  });
+};
+
+watch(columns, () => {
   emit(
     "update",
-    cloneDeep({
-      ...props.board,
-      order: JSON.stringify(toRaw(columns)),
-    })
-  )
-);
+    cloneDeep({ ...board, order: JSON.stringify(toRaw(columns)) })
+  );
+});
+
+const addTask = async ({
+  column,
+  title,
+}: {
+  column: Column;
+  title: string;
+}) => {
+  const newTask = { title };
+  try {
+    const savedTask = await props.addTask(newTask);
+    tasks.push({ ...savedTask });
+    column.taskIds.push(savedTask.id);
+  } catch (error) {
+    alerts.error("Error creating task!");
+  }
+};
 </script>
 
 <template>
-  <button class="text-gray-500" @click="addColumn">New Column +</button>
   <div class="flex items-start py-12">
     <draggable
       :list="columns"
@@ -37,25 +67,36 @@ watch(columns, () =>
         <div
           class="column bg-gray-100 flex flex-col justify-between px-3 py-3 rounded mr-4 w-[300px]"
         >
-          <h2>{{ column.title }}</h2>
-          <draggable
-            :list="column.taskIds"
-            group="tasks"
-            item-key="uid"
-            :animation="200"
-            ghost-class="ghost-card"
-            class="min-h-[400px]"
-          >
-            <template #item="{ element: taskId }">
-              <TaskCard
-                v-if="tasks.find((t) => t.id === taskId)"
-                :task="tasks.find((t) => t.id === taskId)"
-                class="mt-3 cursor-move"
-              />
-            </template>
-          </draggable>
+          <div>
+            <h3>{{ column.title }}</h3>
+            <draggable
+              :list="column.taskIds"
+              group="tasks"
+              item-key="uid"
+              :animation="200"
+              ghost-class="ghost-card"
+              class="min-h-[400px]"
+            >
+              <template #item="{ element: taskId }">
+                <TaskCard
+                  v-if="tasks.find((t: Task) => t.id===taskId)"
+                  :task="(tasks.find((t: Task) => t.id===taskId) as Task)"
+                  class="mt-3 cursor-move"
+                />
+              </template>
+            </draggable>
+            <TaskCreator
+              @create="
+                addTask({
+                  column,
+                  title: $event,
+                })
+              "
+            />
+          </div>
         </div>
       </template>
     </draggable>
+    <button class="text-gray-500">New Column +</button>
   </div>
 </template>
--- src/pages/boards/[id].vue
+++ src/pages/boards/[id].vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { ref, toRefs } from "vue";
+import type { Task } from "@/types";
+import { v4 as uuidv4 } from "uuid";
 import { useAlerts } from "@/stores/alerts";
 import AppPageHeading from "../../components/AppPageHeading.vue";
+import BoardMenu from "../../components/BoardMenu.vue";
 
 const alerts = useAlerts();
 
@@ -11,26 +14,47 @@ const props = defineProps({
 const { id: boardId } = toRefs(props);
 
 const board = ref({
-  id: boardId?.value,
+  id: boardId?.value || "1",
   title: "Let's have an amazing time at Vue.js forge!! 🍍",
   order: JSON.stringify([
     { id: "1", title: "backlog 🌴", taskIds: ["1", "2"] },
   ]),
 });
 
-const tasks = ref([
+const tasks = ref<Partial<Task>[]>([
   { id: "1", title: "Code like mad people!" },
   { id: "2", title: "Push clean code" },
 ]);
+
+const addTask = async (task: Task) => {
+  return new Promise((resolve, reject) => {
+    const taskWithTheId = {
+      ...task,
+      id: uuidv4(),
+    };
+    tasks.value.push(taskWithTheId);
+    resolve(taskWithTheId);
+  });
+};
 
 const updateBoard = (b) => {
   board.value = b;
   alerts.success("Board updated!");
 };
+
+const deleteBoardIfConfirmed = () => {
+  console.log("delete board");
+};
 </script>
 
 <template>
   <AppPageHeading>{{ board.title }}</AppPageHeading>
+  <BoardMenu :board="board" @deleteBoard="deleteBoardIfConfirmed" />
 
-  <BoardDragAndDrop :tasks="tasks" :board="board" @update="updateBoard" />
+  <BoardDragAndDrop
+    :tasks="tasks"
+    :board="board"
+    @update="updateBoard"
+    :addTask="addTask"
+  />
 </template>
